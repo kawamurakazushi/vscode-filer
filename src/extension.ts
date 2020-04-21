58882b496a0d6235b6ab366e86d5c76f089b96c9
Kazushi Kawamura <wakaka2277@gmail.com>
2020-04-21 21:21:48 +0900

    feat: move to directory, and jump to file

--- src/extension.ts
+++ src/extension.ts
@@ -6,67 +6,75 @@ interface Item extends QuickPickItem {
   type: "file" | "directory";
 }
 
+const listFiles = async (searchPath: string): Promise<Item[]> => {
+  const d = new RelativePattern(searchPath, "*/*");
+  const f = new RelativePattern(searchPath, "*");
+
+  const directories = (await workspace.findFiles(d))
+    .map(
+      ({ path }): Item => {
+        const match = /^(.+)\/([^/]+)$/.exec(path);
+        const dir = match ? match[1] : path;
+        return {
+          path: dir,
+          label: path.replace(`${searchPath}/`, "").split("/")[0] + "/",
+          description: "directory",
+          type: "directory",
+        };
+      }
+    )
+    .reduce<Item[]>((acc, cur) => {
+      if (acc.filter(({ label }) => label === cur.label).length > 0) {
+        return acc;
+      }
+      return [...acc, cur];
+    }, []);
+
+  const files: Item[] = (await vscode.workspace.findFiles(f)).map(
+    ({ path }) => ({
+      path: path,
+      label: path.replace(`${searchPath}/`, ""),
+      description: "file",
+      type: "file",
+    })
+  );
+
+  return [...directories, ...files];
+};
+
 export function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension "vscode-filer" is now active!');
 
-  let disposable = vscode.commands.registerCommand(
+  const disposable = vscode.commands.registerCommand(
     "extension.helloWorld",
     async () => {
       const rootPath = vscode.workspace.rootPath;
       const active = vscode.window.activeTextEditor?.document.uri;
 
       if (rootPath) {
-        const d = new RelativePattern(rootPath, "*/*");
-        const f = new RelativePattern(rootPath, "*");
-
-        const directories = (await vscode.workspace.findFiles(d))
-          .map(
-            ({ path }): Item => ({
-              // TODO: fix path
-              path: path,
-              label: path.replace(`${rootPath}/`, "").split("/")[0] + "/",
-              description: "directory",
-              type: "directory",
-            })
-          )
-          .reduce<Item[]>((acc, cur) => {
-            if (acc.filter(({ label }) => label === cur.label).length > 0) {
-              return acc;
-            }
-            return [...acc, cur];
-          }, []);
-
-        const files: Item[] = (await vscode.workspace.findFiles(f)).map(
-          ({ path }) => ({
-            path: path,
-            label: path.replace(`${rootPath}/`, ""),
-            description: "file",
-            type: "file",
-          })
-        );
-
-        const all = [...directories, ...files];
-
         const quickPick = window.createQuickPick<Item>();
-        quickPick.placeholder = "Search";
-        quickPick.items = all;
+        quickPick.placeholder = rootPath;
+
+        quickPick.items = await listFiles(rootPath);
 
         quickPick.onDidAccept(async () => {
           if (quickPick.selectedItems.length > 0) {
             const item = quickPick.selectedItems[0];
             if (item.type === "directory") {
-							window.showInformationMessage("It's a directory");
-							quickPick.items = [];
+              window.showInformationMessage(item.path);
+              quickPick.value = "";
+              quickPick.placeholder = item.path;
+              quickPick.items = await listFiles(item.path);
               return;
             }
+
+            // Jump if it's a file
             const doc = await workspace.openTextDocument(item.path);
             await window.showTextDocument(doc);
           }
         });
 
         quickPick.show();
-
-        console.log(all);
       }
 
       vscode.window.showInformationMessage("Hello World Vscode!");
